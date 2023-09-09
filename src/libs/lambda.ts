2649bef4b6890f65d3d172fb6719323489ddb8f1
HU SHUKANG <hushukang@gmail.com>
2023-09-09 20:54:20 +0900

    add cors settings

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -9,18 +9,18 @@ export const handlerPath = (context: string) => {
   return `${context.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}`;
 };
 
-const addResponseHeaders: any = () => {
-  return {
-    after: (handler: any, next: any) => {
-      handler.response = handler.response || {};
-      handler.response.headers = {
-        ...handler.response.headers,
-        'Access-Control-Allow-Origin': '*'
-      };
-      return next();
-    }
-  };
-};
+// const addResponseHeaders: any = () => {
+//   return {
+//     after: (handler: any, next: any) => {
+//       handler.response = handler.response || {};
+//       handler.response.headers = {
+//         ...handler.response.headers,
+//         'Access-Control-Allow-Origin': '*'
+//       };
+//       return next();
+//     }
+//   };
+// };
 
 export const middyfy = (handler: any, schema?: object) => {
   let func = middy(handler).use(middyJsonBodyParser());
@@ -28,6 +28,6 @@ export const middyfy = (handler: any, schema?: object) => {
     const ajv = transpileSchema(schema, { $data: true, allErrors: true, coerceTypes: false });
     func = func.use(validator({ eventSchema: ajv }));
   }
-  func = func.use(httpErrorHandler()).use(addResponseHeaders()).use(cors());
+  func = func.use(httpErrorHandler()).use(cors());
   return func;
 };
