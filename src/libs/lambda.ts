a6539adc0348b5bc42f53355d8ba24178a3da5d8
HU SHUKANG <hushukang@gmail.com>
2023-09-09 20:29:39 +0900

    add cors settings

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -9,18 +9,18 @@ export const handlerPath = (context: string) => {
   return `${context.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}`;
 };
 
-const addResponseHeaders: any = () => {
-  return {
-    after: (handler: any, next: any) => {
-      handler.response = handler.response || {};
-      handler.response.headers = {
-        ...handler.response.headers,
-        'Access-Control-Allow-Origin': '*'
-      };
-      return next();
-    }
-  };
-};
+// const addResponseHeaders: any = () => {
+//   return {
+//     after: (handler: any, next: any) => {
+//       handler.response = handler.response || {};
+//       handler.response.headers = {
+//         ...handler.response.headers,
+//         'Access-Control-Allow-Origin': '*'
+//       };
+//       return next();
+//     }
+//   };
+// };
 
 export const middyfy = (handler: any, schema?: object) => {
   let func = middy(handler).use(middyJsonBodyParser());
