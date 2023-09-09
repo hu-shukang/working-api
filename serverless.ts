c403493dddeec5d5165e22d157579316c31044f4
HU SHUKANG <hushukang@gmail.com>
2023-09-09 23:17:41 +0900

    serverless-layers

--- serverless.ts
+++ serverless.ts
@@ -1,7 +1,14 @@
 import type { AWS } from '@serverless/typescript';
 import { WorkingTable } from '@resources/dynamodb';
 import { getToken } from '@functions/index';
+import { readFileSync } from 'fs';
+import * as path from 'path';
 
+const packageJson = JSON.parse(readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+const allDependencies = [
+  ...Object.keys(packageJson.dependencies || {}),
+  ...Object.keys(packageJson.devDependencies || {})
+];
 const serverlessConfiguration: AWS = {
   service: 'working-api',
   frameworkVersion: '3',
@@ -33,6 +40,7 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
+      external: allDependencies,
       exclude: ['aws-sdk'],
       target: 'node18',
       define: { 'require.resolve': undefined },
@@ -40,8 +48,9 @@ const serverlessConfiguration: AWS = {
       concurrency: 10
     },
     'serverless-layers': {
-      layersDeploymentBucket: 'working-api-deploy',
-      dependenciesPath: './package.json'
+      common: {
+        dependenciesPath: './package.json'
+      }
     }
   }
 };
