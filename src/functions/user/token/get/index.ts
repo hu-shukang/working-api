041847309f2566577d155823abb493d2fe309893
HU SHUKANG <hushukang@gmail.com>
2023-09-09 20:35:23 +0900

    add cors settings

--- src/functions/user/token/get/index.ts
+++ src/functions/user/token/get/index.ts
@@ -8,6 +8,7 @@ export default {
       http: {
         method: 'post',
         path: '/user/token',
+        cors: true,
         request: {
           schemas: {
             'application/json': schema
